b714b512ab86cc27e3c4a843e3dbca8a5e567b25
Matt Schmale <MattSchmale1@gmail.com>
2023-03-07 21:25:08 -0600

    Removing unused packages

--- hfCLI.py
+++ hfCLI.py
@@ -1,12 +1,9 @@
 #!/us/bin/env python
 
-import click
 from transformers import pipeline
 import urllib.request
 from bs4 import BeautifulSoup
-import wikipedia
 
-# make a function that extracts the text from a url
 def extract_text(url):
     page = urllib.request.urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
@@ -14,7 +11,6 @@ def extract_text(url):
     return text
 
 
-# write a function that uses hugging face to return a summary of the text
 def summarize(text):
     summarizer = pipeline("summarization", model="t5-small")
     summary = summarizer(text, max_length=180)
